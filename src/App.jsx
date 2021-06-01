9fc171fcadb5f68f6473b120dad1b1c2835eaaaf
Nathan Phame <diyf2021@gmail.com>
2021-06-01 01:34:19 -0400

    Basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import React, { Component } from 'react'
 
 export class App extends Component {
+  state = {}
   render() {
-    return <div>Hello, World!</div>
+    return (
+      <main>
+        <h1>Movie Database</h1>
+        <ul>
+          <li>
+            <input type="text" value={this.state.id} />
+          </li>
+          <li>Movie Title</li>
+          <li>Poster Image</li>
+          <li>Plot Overview</li>
+        </ul>
+      </main>
+    )
   }
 }
